a3f965ca23d025da3d88fd49b5aa73854a2b5416
John Merz <jmerz826@gmail.com>
2021-11-15 18:24:24 -0500

    login Styles page added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import HomePage from './components/home-page';
+import PotluckForm from './components/potluck-form';
+import LoginPage from './components/login-page';
+import LoginPageStyle from './components/login-page-style';
+import RegisterPage from './components/register-page';
 
 function App() {
   return (
     <div className="App">
+      <HomePage />
+      <PotluckForm />
+      <LoginPage />
+      <RegisterPage />
+      <LoginPageStyle />
     </div>
   );
 }
---
+++ src/components/login-page-style.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LoginPageStyle = (props) => {
+
+}
+
+export default LoginPageStyle;
