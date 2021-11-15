b258630b9a3da4b5a003b729678015e8510b0bea
John Merz <jmerz826@gmail.com>
2021-11-15 18:43:44 -0500

    switch in app set up

--- src/App.js
+++ src/App.js
@@ -5,15 +5,29 @@ import PotluckForm from './components/potluck-form';
 import LoginPage from './components/login-page';
 import LoginPageStyle from './components/login-page-style';
 import RegisterPage from './components/register-page';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
-      <PotluckForm />
-      <LoginPage />
-      <RegisterPage />
-      <LoginPageStyle />
+      <Switch>
+        <Route exact path='/'>
+          <HomePage />
+        </Route>
+        <Route path='/potluck-form'>
+          <PotluckForm />
+        </Route>
+        <Route path='/login'>
+          <LoginPage />
+        </Route>
+        <Route path='/register'>
+          <RegisterPage />
+        </Route>
+        <Route path='/style'>
+          <LoginPageStyle />
+        </Route>
+      </Switch>
+      
     </div>
   );
 }
