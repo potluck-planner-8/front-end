1250885c0f88c8435e7dc3a6ab4f42907c218d80
Leo Oladimu <leooladimu9@gmail.com>
2021-11-15 20:23:40 -0500

    LoginPage

--- src/components/login-page.js
+++ src/components/login-page.js
@@ -1,7 +1,117 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import * as yup from 'yup';
+import schema from './formSchema';
+import axios from 'axios';
+
+const initialFormValues = {
+    username: '',
+    password: ''
+}
+
+const initialFormErrors = {
+  username: '',
+  password: ''
+}
+
+const initialDisabled = true
 
 const LoginPage = (props) => {
-  return <h1>hi</h1>;
-};
+
+  const [users, setUsers] = useState([])  
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const [formErrors, setFormErrors] = useState(initialFormErrors)
+  const [disabled, setDisabled] = useState(initialDisabled) 
+
+  const validate = (name, value) => {
+    yup.reach(schema, name)
+      .validate(value)
+      .then(() => setFormErrors({ ...formErrors, [name]: '' }))
+      .catch(err => setFormErrors({ ...formErrors, [name]: err.errors[0] }))
+  }
+
+  console.log(formErrors);
+
+  const postUser = newUser => {
+    axios.post('https://potluck-planner-8.herokuapp.com/api/users', newUser)
+      .then(r => {
+        setUsers([r.data, ...users]);
+        setFormValues(initialFormValues);
+      }) 
+      .catch(err => {
+        console.error(err);
+        setFormValues(initialFormValues);
+      })
+  }
+
+  const onChange = e => {
+    const { name, value } = e.target;
+    validate(name, value)
+    setFormValues({ ...formValues, [name] : value});
+  }
+
+  const formSubmit = (e) => {
+    e.preventDefault();
+    const newClient = {
+      username: formValues.username.trim(),
+      password: formValues.password.trim()
+    }
+
+    // postUser(newUser)
+    // newUser as param ↙
+    return setUsers()
+  }
+  
+  useEffect(() => {
+    schema.isValid(formValues).then(valid => setDisabled(!valid))
+  }, [formValues])
+  
+  return (
+    <div className='login-box'>
+      <h2>Login</h2>
+      <form id='login-form'onSubmit={formSubmit}>
+        <div className='user-box'>
+          <input
+            id="username"
+            value={formValues.username}
+            name="username"
+            type="text"
+            onChange={onChange}
+          />
+          <label>Username</label>
+          { formErrors.username.length > 0 ? <p style={{color: 'orange'}}>{ formErrors.username }</p> : null }
+        </div>
+        <div className='user-box'>
+          <input
+            id=""
+            value={formValues.password}
+            name="name" 
+            type="text"
+            onChange={onChange}
+          />
+          <label>Password</label>
+          { formErrors.password.length > 0 ? <p style={{color: 'orange'}}>{ formErrors.password }</p> : null }
+        </div>
+        <div className='user-box'>
+          <input
+            id="password"
+            value={formValues.password}
+            name="password" 
+            type="password"
+            onChange={onChange}
+          />
+       </div>
+       
+        {/* <a href='submit-button' type='submit' disabled={disabled}>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          Submit!
+        </a> */}
+
+      </form>
+    </div>
+  )
+}
 
 export default LoginPage;
