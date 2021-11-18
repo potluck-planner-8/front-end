116d43ba2bd6bb5c791ebd9fb1f41f1fec01ff85
Leo Oladimu <leooladimu9@gmail.com>
2021-11-18 18:19:18 -0500

    RegisterForm

---
+++ src/components/formSchemaRegister.js
@@ -0,0 +1,28 @@
+import * as yup from 'yup';
+
+const formSchema = yup.object().shape({
+    name: yup
+        .string()
+        .trim()
+        .required('Full Name is required!')
+        .min(1),
+    email: yup
+        .string()
+        .email('Must be a valid email address!')
+        .required('Email is required!'),
+    username: yup
+        .string()
+        .trim()
+        .required('Username is required!')
+        .min(1),
+    password: yup
+        .string()
+        .trim()
+        .required('Password is required!')
+        .min(1),
+    role_name: yup
+        .string()
+        .oneOf(['client'], 'Role is required!'),
+})
+
+export default formSchema;
