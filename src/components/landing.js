c0474e17891c76c381a42a54d3dd877669ae7139
Leo Oladimu <leooladimu9@gmail.com>
2021-11-18 16:19:46 -0500

    Logout2

--- src/components/landing.js
+++ src/components/landing.js
@@ -5,11 +5,11 @@ import axiosWithAuth from '../utils/axiosWithAuth';
 const Landing = (props) => {
     const [potlucks, setPotlucks] = useState([]);
 
-    useEffect(()=> {
+    useEffect(() => {
         axiosWithAuth()
             .get('https://potluck-planner-8.herokuapp.com/api/potlucks')
             .then(res=>{
-            console.log(res)
+                console.log(res)
             setPotlucks(res.data)
             })
             .catch(err=>{
@@ -17,29 +17,22 @@ const Landing = (props) => {
             })
     }, [])
 
-
-    
-
-
-
     return (
         <div>
         <Link to='/potluck-form'><button>Create Potluck!</button></Link>
             <div>
                 {potlucks.map( potluck => {
-                return(
-                    <div>
-                    <h2>{potluck.location} </h2>
-                    {potluck.date}
-                    {potluck.time}
-                    </div>
-            )
-            })}
+                    return(
+                        <div>
+                        <h2>{potluck.location} </h2>
+                        {potluck.date}
+                        {potluck.time}
+                        </div>
+                    )
+                })}
             </div>
         </div>
     )
 }
-
-
 
 export default Landing;
--- src/components/logout.js
+++ src/components/logout.js
@@ -2,13 +2,13 @@ import React, { useEffect } from 'react';
 import axiosWithAuth from '../utils/axiosWithAuth';
 
 export const Logout = () => {  
-   
+   console.log('%∞')
     useEffect(() => {
         axiosWithAuth()
-            .post('https://potluck-planner-8.herokuapp.com/api/auth/logout')
+            .post('https://potluck-planner-8.herokuapp.com/api')
             .then(r => {
                 console.log('£', r)
-                localStorage.removeItem('token');
+                // localStorage.removeItem('token');
             }).catch((err) => console.log(err.message));
     },[]);
 
@@ -17,6 +17,6 @@ export const Logout = () => {
             <button className='logout' onClick={handleDelete}>Logout</button>
         </div>
     )
-}
+};
 
 // export default Logout
